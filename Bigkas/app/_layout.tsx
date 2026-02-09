b1123f0bd1a38b55f7cdf8ca02233e00202cce3f
LAPTOP-4TJAVEAO\maria <cmariahangela@gmail.com>
2026-02-08 18:02:03 -0600

    Add placeholder screen with "Coming Soon" title

--- Bigkas/app/_layout.tsx
+++ Bigkas/app/_layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="placeholder" options={{ title: "Coming Soon" }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>

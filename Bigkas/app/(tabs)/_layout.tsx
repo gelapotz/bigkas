8842e67e1006cb51d103d8bf9799abed72557d89
LAPTOP-4TJAVEAO\maria <cmariahangela@gmail.com>
2026-02-08 21:43:14 -0600

    Update tab layout, specifically the colors

--- Bigkas/app/(tabs)/_layout.tsx
+++ Bigkas/app/(tabs)/_layout.tsx
@@ -12,22 +12,34 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+
+        tabBarStyle: {
+          backgroundColor: '#0A4CB8',
+        },
+
+        tabBarActiveTintColor: '#FCD116',
+
+        tabBarInactiveTintColor: '#FFFFFF',
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
         }}
       />
     </Tabs>

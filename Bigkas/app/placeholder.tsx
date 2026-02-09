f53ae0f141121b557dbcefb292f3a41d702ae022
LAPTOP-4TJAVEAO\maria <cmariahangela@gmail.com>
2026-02-08 21:00:27 -0600

    Update placeholder styles (mainly the color)

--- Bigkas/app/placeholder.tsx
+++ Bigkas/app/placeholder.tsx
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F9F9F9',
+    backgroundColor: '#E3EDFF',
   },
   title: {
     fontSize: 24,
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
   },
   subtitle: {
     fontSize: 16,
-    color: '#444',
+    color: '#777',
     marginTop: 8,
   },
 });
